955ec4cfcbc258b1b36fc374113985fd2b531f62
Precession <precession.cv01@gmail.com>
2021-05-08 11:58:54 +0900

    Addition sample : over 2 elements ver.

--- cpp_workspace/999_cheatsheets/GCD_LCM/main.cpp
+++ cpp_workspace/999_cheatsheets/GCD_LCM/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -9,5 +10,22 @@ int main() {
     cout << __gcd(a, b) << endl;
     // Least common multiple
     cout << a / __gcd(a, b) * b << endl;
+
+    ////// Over 2 elements version /////
+    vector<int> num_arr{12, 24, 36};
+    // Gratest common divisor
+    int gcd = num_arr[0];
+    for(int i = 1; i < num_arr.size(); i++) {
+        gcd = __gcd(gcd, num_arr[i]);
+    }
+    cout << gcd << endl;
+    // Least common multiple
+    int lcm = num_arr[0];
+    for(int i = 1; i < num_arr.size(); i++) {
+        lcm = lcm / __gcd(lcm, num_arr[i]) * num_arr[i];
+    }
+    cout << lcm << endl;
+
     return 0;
+
 }
