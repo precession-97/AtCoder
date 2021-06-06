aa6fd09a74eb1082f05c0f945776fe80f557ba2b
Precession <precession.cv01@gmail.com>
2021-06-06 20:54:53 +0900

    updated abc203 (not completed)

--- cpp_workspace/abc203/D/main.cpp
+++ cpp_workspace/abc203/D/main.cpp
@@ -1,36 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long sub_solve(long long K, vector<long long> pond) {
-    sort(pond.begin(), pond.end());
-    int target_idx = K*K - floor(K*K/2) - 1;
-    return pond[target_idx];
-}
-
 void solve(long long N, long long K, std::vector<std::vector<long long>> A){
     /*
         N       : num of area (N x N)
         K       : size of pond (K x K)
         A[][]   : 
     */
-    long long min_med = 1000000001;
-    for(int di = 0; di < (N-K+1); di++) {
-        for(int dj = 0; dj < (N-K+1); dj++) {
-            vector<long long> pond(K*K);
-            pair<int, int> up_left = {di, dj};
-            pair<int, int> dw_rght = {di + K - 1, dj + K - 1};
-            int idx = 0;
-            for(int i = up_left.first; i <= dw_rght.first; i++) {
-                for(int j = up_left.second; j <= dw_rght.second; j++) {
-                    pond[idx] = A[i][j];
-                    idx++;
+    vector<vector<long long>> prefix_sum_2d(N+1, vector<long long>(N+1, 0));
+
+    /* bin search */
+    int ok = 100000000;
+    int ng = -1;
+    int lim = ((K*K)/2) + 1
+    while(ng + 1 < ok) {
+        long long med = (ok + ng) / 2;
+        /* make array of prefix sum*/
+        for(int h = 0; h < N; h++) {
+            for(int w = 0; w < N; w++) {
+                prefix_sum_2d[h+1][w+1] = prefix_sum_2d[h][w+1] + prefix_sum_2d[h+1][w] - prefix_sum_2d[h][w];
+                if(A[h][w] >= med) {
+                    prefix_sum_2d[h+1][w+1] = prefix_sum_2d[h+1][w+1] + 1;
                 }
             }
-            long long med = sub_solve(K, move(pond));
-            min_med = min(min_med, med);
+        }
+        bool is_exist = false;
+        for(int kh = 0; kh < N-K+1; kh++) {
+            for(int kw = 0; kw < N-K+1; kw++) {
+                int cnt = prefix_sum_2d[kh+K][kw+K] - prefix_sum_2d[kh+K][kw] - prefix_sum_2d[kh][kw+K] + prefix_sum_2d[kh][kw];
+                if(cnt >= lim) {
+                    is_exist = true;
+                    break;
+                }
+            }
+        }
+        if(is_exist) {
+            ng = med;
+        } else {
+            ok = med;
         }
     }
-    cout << min_med << endl;
+    cout << ok << endl;
 }
 
 int main(){
