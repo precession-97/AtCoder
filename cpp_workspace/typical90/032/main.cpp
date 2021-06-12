864be3917f771798a0bc7e5cf31050b4346be4ac
Precession <precession.cv01@gmail.com>
2021-06-12 23:06:37 +0900

    modified 032 to AC

--- cpp_workspace/typical90/032/main.cpp
+++ cpp_workspace/typical90/032/main.cpp
@@ -14,17 +14,35 @@ void solve(long long N, std::vector<std::vector<long long>> A, long long M, std:
     for(int idx = 0; idx < N; idx++) {
         idxv[idx] = idx;
     }
-    vector<vector<bool>> is_NG(N, vector<bool>(N, false));
-    long long min_time = 1000 * (N + 1);
-    for(int n = 0; n < N; n++) {
-        for(int o = 0; o < N; o++) {
-            cout << is_NG[n][o] << " ";
-        }
-        cout << endl;
-    }
-    // do {
 
-    // } while(next_permutation(idxv.begin(), idxv.end()));
+    vector<vector<bool>> is_NG(N, vector<bool>(N, false));
+    for(int i = 0; i < M; i++) {
+        is_NG[X[i]-1][Y[i]-1] = true;
+        is_NG[Y[i]-1][X[i]-1] = true;
+    }
+
+    long long min_time = 1000 * (N + 1);
+    do {
+        long long time = A[idxv[0]][0];
+        bool ok = true;
+        for(int i = 1; i < N; i++) {
+            if(is_NG[idxv[i]][idxv[i-1]]) {
+                ok = false;
+                break;
+            }
+            time += A[idxv[i]][i];
+        }
+        if(ok) {
+            min_time = min(time, min_time);
+        }
+    } while(next_permutation(idxv.begin(), idxv.end()));
+    
+    if(min_time == 1000*(N+1)) {
+        cout << -1 << endl;
+    } else {
+        cout << min_time << endl;
+    }
+
 }
 
 int main(){
