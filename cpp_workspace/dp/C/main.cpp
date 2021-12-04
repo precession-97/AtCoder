c2db93c2f0ec74bbfde7ca5fb7a8cc85a073f5e6
precession-97 <precession.cv01@gmail.com>
2021-12-04 15:42:29 +0900

    update C to AC

--- cpp_workspace/dp/C/main.cpp
+++ cpp_workspace/dp/C/main.cpp
@@ -19,8 +19,39 @@
 #include <cassert>
 using namespace std;
 
+template<class T> inline bool chmax(T& a, T b);
 
 void solve(long long N, std::vector<long long> a, std::vector<long long> b, std::vector<long long> c){
+    // A, B, C
+    const int KIND_ACTION = 3;
+    // DP table
+    long long dp[N][KIND_ACTION];
+    // dp init
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < KIND_ACTION; j++) {
+            dp[i][j] = 0;
+        }
+    }
+    // initial condition
+    dp[0][0] = a[0];
+    dp[0][1] = b[0];
+    dp[0][2] = c[0];
+
+    vector<vector<long long>> happiness = {a, b, c};
+
+    for(int i = 1; i < N; i++) {
+        // j : selected action-idx on N_i
+        for(int j = 0; j < KIND_ACTION; j++) {
+            // k : selected action-idx on N_(i-1)
+            for(int k = 0; k < KIND_ACTION; k++) {
+                if(j != k) {
+                    chmax(dp[i][j], dp[i-1][k] + happiness[j][i]);
+                }
+            }
+        }
+    }
+    
+    cout << max({dp[N-1][0], dp[N-1][1], dp[N-1][2]}) << endl;
 
 }
 
@@ -38,4 +69,12 @@ int main(){
     }
     solve(N, std::move(a), std::move(b), std::move(c));
     return 0;
+}
+
+template<class T> inline bool chmax(T& a, T b) {
+    if(a < b) {
+        a = b;
+        return true;
+    }
+    return false;
 }
