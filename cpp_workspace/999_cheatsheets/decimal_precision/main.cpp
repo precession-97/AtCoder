acd03871344052815b45b67f1c694a519fbc4c23
precession-97 <precession.cv01@gmail.com>
2023-06-11 16:12:00 +0900

    Update decimal_precision

--- cpp_workspace/999_cheatsheets/decimal_precision/main.cpp
+++ cpp_workspace/999_cheatsheets/decimal_precision/main.cpp
@@ -5,12 +5,19 @@ using namespace std;
 
 int main() {
 
+    // 小数点以下14桁
     double pi = 3.14159265358979;
 
     // cout (default)
     cout << "[default cout] pi = " << pi << endl;
 
     // cout (setprecision)
-    cout << "[setpresicion] pi = " << setprecision(15) << pi << endl;
+    cout << "[setpresicion] pi = " << setprecision(14) << pi << endl;
+    // 3.1415926535898 (整数部を含めて14桁)
+
+    // cout (setprecision)
+    cout << "[with fix] pi = " << fixed << setprecision(14) << pi << endl;
+    // 3.14159265358979 (少数点以下14桁)
+
     return 0;
 }
